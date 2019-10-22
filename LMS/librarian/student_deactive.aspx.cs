1b60319abe3c87a729dba5d30822a2929be3e0b6
Amarsh28 <amartya287@gmail.com>
2019-10-22 12:36:33 +1100

    student deactive 1222

--- LMS/librarian/student_deactive.aspx.cs
+++ LMS/librarian/student_deactive.aspx.cs
@@ -11,7 +11,21 @@ namespace LMS.librarian
 {
     public partial class student_deactive : System.Web.UI.Page
     {
-        
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\lms.mdf;Integrated Security=True");
+
+        int id;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+           
+            if (con.State == System.Data.ConnectionState.Open)
+            {
+                con.Close();
+            }
+            con.Open();
+
+            if (Session["librarian"] == null)
+            {
+                Response.Redirect("login.aspx");
             } 
 
             id = Convert.ToInt32(Request.QueryString["id"].ToString());
