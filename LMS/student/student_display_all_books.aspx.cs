99cfa9f292c8803c4c434241f7db6f40bd1e6a3c
Amarsh28 <amartya287@gmail.com>
2019-10-22 12:53:50 +1100

    display all books2

--- LMS/student/student_display_all_books.aspx.cs
+++ LMS/student/student_display_all_books.aspx.cs
@@ -15,6 +15,45 @@ namespace LMS.student
 
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (con.State == System.Data.ConnectionState.Open)
+            {
+                con.Close();
+            }
+            con.Open();
+
+            if (Session["student"] == null)
+            {
+                Response.Redirect("student_login.aspx");
+            }
+
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from books";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            r1.DataSource = dt;
+            r1.DataBind();
+
+
+        }
+
+
+
+        public string checkvideo(object myvalue, object id)
+        {
+            if (myvalue == null)
+            {
+                return "Not Avaliable";
+              
+            }
+            else
+            {
+                myvalue = "../librarian/" + myvalue.ToString();
+                return "<a href ='"+myvalue.ToString()+ "' style ='color:green'>view video </a>";
+            }
+
 
         }
 
