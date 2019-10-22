9353f6f3f2b62ed6c1b0df795353158cc7b300fe
Amarsh28 <amartya287@gmail.com>
2019-10-22 12:38:25 +1100

    student active 2

--- LMS/librarian/student_active.aspx.cs
+++ LMS/librarian/student_active.aspx.cs
@@ -12,5 +12,14 @@ namespace LMS.librarian
     public partial class student_active : System.Web.UI.Page
     {
        
+            id = Convert.ToInt32(Request.QueryString["id"].ToString());
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "update student_registration set approved = 'yes' where id ="+id+"";
+            cmd.ExecuteNonQuery();
+         
+
+            Response.Redirect("display_student_info.aspx");
+        }
     }
 }
