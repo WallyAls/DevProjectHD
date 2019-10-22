caf792e8114f61a530a55ca8f3b4cb4faa33da1e
Amarsh28 <amartya287@gmail.com>
2019-10-22 12:50:43 +1100

    my issued books1

--- LMS/student/my_issued_books.aspx.cs
+++ LMS/student/my_issued_books.aspx.cs
@@ -17,6 +17,34 @@ namespace LMS.student
         double noofdays = 0;
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+            con.Open();
+            if (Session["student"] == null)
+            {
+                Response.Redirect("student_login.aspx");
+            }
+
+
+
+            SqlCommand cmd2 = con.CreateCommand();
+            cmd2.CommandType = CommandType.Text;
+            cmd2.CommandText = "select * from Penalty1";
+            cmd2.ExecuteNonQuery();
+            DataTable dt2 = new DataTable();
+            SqlDataAdapter da2 = new SqlDataAdapter(cmd2);
+            da2.Fill(dt2);
+            foreach (DataRow dr2 in dt2.Rows)
+            {
+                penalty = dr2["Penalty1"].ToString();
+            }
+
+
+
+
+
         }
     }
 }
