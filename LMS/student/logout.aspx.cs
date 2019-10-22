20f423ab1609987258257023b17e3257bf3ebac8
Amarsh28 <amartya287@gmail.com>
2019-10-22 12:47:59 +1100

    logut for students

--- LMS/student/logout.aspx.cs
+++ LMS/student/logout.aspx.cs
@@ -9,10 +9,6 @@ namespace LMS.student
 {
     public partial class logout : System.Web.UI.Page
     {
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            Session.Clear();
-            Response.Redirect("student_login.aspx");
-        }
+      
     }
 }
