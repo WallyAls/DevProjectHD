000c113a3ac5e5fc00ee45ea31a45dadbc7a5dec
Amarsh28 <amartya287@gmail.com>
2019-10-22 11:52:10 +1100

    updating displaying all books

--- LMS/librarian/display_all_books.aspx.cs
+++ LMS/librarian/display_all_books.aspx.cs
@@ -12,6 +12,37 @@ namespace LMS.librarian
     public partial class display_all_books : System.Web.UI.Page
     {
 
+        SqlConnection con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\LMS\LMS\LMS\App_Data\lms.mdf;Integrated Security=True");
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            if (con.State == System.Data.ConnectionState.Open)
+            {
+                con.Close();
+            }
+            con.Open();
+            if (Session["librarian"] == null)
+            {
+                Response.Redirect("login.aspx");
+            }
+
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "select * from books";
+            cmd.ExecuteNonQuery();
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            da.Fill(dt);
+            r1.DataSource = dt;
+            r1.DataBind();
+
+
+
+
+
+        }
+		
 		//checks if myvalue has a value and returns the appropriate pdf
         public string checkvideo(object myvalue, object id)
         {
