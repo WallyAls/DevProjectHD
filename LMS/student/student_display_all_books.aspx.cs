ce89e03ee6a2024a9536bade026160f53ca3b0b9
Amarsh28 <amartya287@gmail.com>
2019-10-22 12:51:42 +1100

    display all books

--- LMS/student/student_display_all_books.aspx.cs
+++ LMS/student/student_display_all_books.aspx.cs
@@ -15,59 +15,11 @@ namespace LMS.student
 
         protected void Page_Load(object sender, EventArgs e)
         {
-            if (con.State == System.Data.ConnectionState.Open)
-            {
-                con.Close();
-            }
-            con.Open();
-
-            if (Session["student"] == null)
-            {
-                Response.Redirect("student_login.aspx");
-            }
-
-            SqlCommand cmd = con.CreateCommand();
-            cmd.CommandType = CommandType.Text;
-            cmd.CommandText = "select * from books";
-            cmd.ExecuteNonQuery();
-            DataTable dt = new DataTable();
-            SqlDataAdapter da = new SqlDataAdapter(cmd);
-            da.Fill(dt);
-            r1.DataSource = dt;
-            r1.DataBind();
-
-
-        }
-
-
-
-        public string checkvideo(object myvalue, object id)
-        {
-            if (myvalue == null)
-            {
-                return "Not Avaliable";
-              
-            }
-            else
-            {
-                myvalue = "../librarian/" + myvalue.ToString();
-                return "<a href ='"+myvalue.ToString()+ "' style ='color:green'>view video </a>";
-            }
-
 
         }
 
         public string checkpdf(object myvalue1, object id1)
         {
-            if (myvalue1 == null)
-            {
-                return "Not Avalibale";
-            }
-            else
-            {
-                myvalue1 = "../librarian/" + myvalue1.ToString();
-                return "<a href ='" + myvalue1.ToString() + "' style ='color:green'>view pdf </a>";
-            }
 
 
         }
