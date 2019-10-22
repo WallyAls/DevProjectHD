42e4ef25b94e0427ed2ee0b91010c5723001d6e4
Amarsh28 <amartya287@gmail.com>
2019-10-22 12:29:55 +1100

    logout section

--- LMS/librarian/logout.aspx.cs
+++ LMS/librarian/logout.aspx.cs
@@ -10,9 +10,6 @@ namespace LMS.librarian
     public partial class logout : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
-        {
-            Session.Clear();
-            Response.Redirect("login.aspx");
-        }
+       
     }
 }
